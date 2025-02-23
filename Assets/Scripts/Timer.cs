90171b0015b821424137cb8db426e7dcceec3023
kmimin <k.t1109kyari@icloud.com>
2025-02-23 16:08:29 +0900

    imageの変更・追加

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameTimer : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     public float timeLeft = 0;
     [SerializeField, Range(1f, 1000f)]float timeLimit = 90f;
